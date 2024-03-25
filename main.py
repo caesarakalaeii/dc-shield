7cdabe6387a48725967e98767c0bf29d104d22c3
caesarakalaeii <caesarlp@gmx.de>
2024-03-25 21:31:43 +0100

    added better logging

--- main.py
+++ main.py
@@ -10,40 +10,19 @@ Dependencies:
 
 """
 
-
+import requests
+from logger import Logger
 from json_handler import *
 from quart import Quart, redirect, request
 
 app = Quart(__name__)
-default_subdomain:str
+l = Logger(console_log= True, file_logging=True, file_URI='logs/log.txt', override=True)
+default_server:str
 alternative_server_url:str
 
 
 
 
-
-@app.route('/')
-async def index():
-    """
-    Index route handler to redirect users based on their country of origin.
-    """
-    global default_subdomain, alternative_server_url, reader
-    
-    ip_address = request.remote_addr
-    try:
-        # Get country code from IP address
-        ip_address = request.remote_addr
-        country_code = await get_country(ip_address)
-
-        # Check if IP address is in Pakistan or India
-        if country_code in ['PK', 'IN']:
-            return redirect(alternative_server_url)
-    except Exception as e:
-        print(f"Error: {e}")
-    
-    # Redirect to default subdomain for other countries
-    default_url = f"{default_subdomain}{request.path}"
-    return redirect(default_url)
 
 async def get_country(ip_address):
     """Retrieve country information for the given IP address."""
@@ -53,19 +32,33 @@ async def get_country(ip_address):
             country_code = response.get("country_code2")
             return country_code
     except Exception as e:
-        print(f"Error fetching country information: {e}")
+        l.error(f"Error fetching country information: {e}")
     return None
 
 async def request_ip_location(ip_address):
     """Make a request to the IP location API."""
     url = f"https://api.iplocation.net/?cmd=ip-country&ip={ip_address}"
     try:
-        response = await request.get(url)
+        response = requests.get(url)
         if response.status_code == 200:
             return response.json()
     except Exception as e:
-        print(f"Error fetching IP location: {e}")
+        l.error(f"Error fetching IP location: {e}")
     return None
+
+@app.route('/')
+async def index():
+    ip_address = request.remote_addr
+    country_code = await get_country(ip_address)
+    l.info(f'Country code is: {country_code}')
+    if country_code and country_code in ['PK', 'IN', 'DE']:
+        return redirect(alternative_server_url)
+    else:
+        
+        print(default_server)
+        return redirect(default_server)
+    
+
 
 
 if __name__ == '__main__':
@@ -74,7 +67,7 @@ if __name__ == '__main__':
     
     
     
-    default_subdomain = config['default_server']
+    default_server = config['default_server']
 
     alternative_server_url = config['honeypot_server']
     
